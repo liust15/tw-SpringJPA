bd78d0f14513e815423b1d7acfa94499b6376577
liust15 <1316431084@qq.com>
2018-05-04 11:13:01 +0800

    提交

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,50 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
 public class Company {
+    @Id
+    private int id;
+    private String companyName;
+    private int employee;
 
+    public Company(int id, String companyName, int employee) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", employee=" + employee +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(int employee) {
+        this.employee = employee;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,86 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
 public class Employee {
+    @Id
+    private int id;
+    private int companyId;
+    private int salary;
+    private String gender;
+    private int age;
+    private String name;
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", companyId=" + companyId +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Employee( String name,int age, String gender, int salary,int companyId, int id ) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
 }
